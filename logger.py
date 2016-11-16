7c8272cca09450c338431add5d622e95a90d4754
thatsIch <thatsIch@mail.de>
2016-11-16 19:00:36 +0100

    Added logger facility

---
+++ logger.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import os
+
+import sublime
+
+__log = None 
+
+# Called automatically from ST3 if plugin is loaded
+# Is required now due to async call and ignoring sublime.* from main routine
+def plugin_loaded():
+	global __log
+
+	key = "rainmeter_enable_logging"
+
+	settings = sublime.load_settings("Rainmeter.sublime-settings")
+	__log = settings.get(key, False)
+	settings.add_on_change(key, __load_settings)
+
+	log(__file__, "plugin_loaded()", "Logger succesfully loaded.")
+
+def __load_settings():
+	settings = sublime.load_settings("Rainmeter.sublime-settings")
+	__log = settings.get(key, False)
+
+def log(file, function, string):
+	if __log:
+		now = datetime.now()
+		timestamp = now.strftime("%H:%M:%S.%f")[:-3]
+
+		filename = os.path.basename(file)
+		withoutext = os.path.splitext(filename)[0]
+
+		print("[" + timestamp + "]", withoutext + "." + function + ':', string)
