93f9bb4115c4259dd38962229947b87e952a25a7
thatsIch <thatsIch@mail.de>
2016-12-12 14:03:59 +0100

    Use better variable names for algorithmic determination

--- completion/levenshtein.py
+++ completion/levenshtein.py
@@ -1,18 +1,18 @@
-def levenshtein(s1, s2):
-    if len(s1) < len(s2):
-        return levenshtein(s2, s1)
+def levenshtein(top_string, bot_string):
+    if len(top_string) < len(bot_string):
+        return levenshtein(bot_string, top_string)
 
     # len(s1) >= len(s2)
-    if len(s2) == 0:
-        return len(s1)
+    if len(bot_string) == 0:
+        return len(top_string)
 
-    previous_row = range(len(s2) + 1)
-    for i, c1 in enumerate(s1):
+    previous_row = range(len(bot_string) + 1)
+    for i, top_char in enumerate(top_string):
         current_row = [i + 1]
-        for j, c2 in enumerate(s2):
+        for j, bot_char in enumerate(bot_string):
             insertions = previous_row[j + 1] + 1  # j+1 instead of j since previous_row and current_row are one character longer
-            deletions = current_row[j] + 1        # than s2
-            substitutions = previous_row[j] + (c1 != c2)
+            deletions = current_row[j] + 1        # than bot_string
+            substitutions = previous_row[j] + (top_char != bot_char)
             current_row.append(min(insertions, deletions, substitutions))
         previous_row = current_row
 
