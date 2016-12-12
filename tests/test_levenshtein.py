93e26824e39d1b191f6a3f58047b37a9f24f057c
thatsIch <thatsIch@mail.de>
2016-12-12 14:16:14 +0100

    Shorten levenshtein test method names

--- tests/test_levenshtein.py
+++ tests/test_levenshtein.py
@@ -5,24 +5,24 @@ from unittest import TestCase
 levenshtein = sys.modules["Rainmeter.completion.levenshtein"]
 
 
-class TestFunctions(TestCase):
+class TestLevenShtein(TestCase):
 
-    def test_levenshtein_same_word_should_equal_zero(self):
+    def same_word_should_equal_zero(self):
         diff = levenshtein.levenshtein("hello", "hello")
 
         self.assertEqual(diff, 0)
 
-    def test_levenshtein_zero_words_should_equal_zero(self):
+    def zero_words_should_equal_zero(self):
         diff = levenshtein.levenshtein("", "")
 
         self.assertEqual(diff, 0)
 
-    def test_levenshtein_first_word_one_longer_should_equal_one(self):
+    def first_word_one_longer_should_equal_one(self):
         diff = levenshtein.levenshtein("hello", "hell")
 
         self.assertEqual(diff, 1)
 
-    def test_levenshtein_second_word_one_longer_should_equal_one(self):
+    def second_word_one_longer_should_equal_one(self):
         diff = levenshtein.levenshtein("hell", "hello")
 
         self.assertEqual(diff, 1)
