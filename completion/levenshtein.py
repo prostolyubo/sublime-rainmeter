7752d9f660d41dfeb3310c4d04fc5da8dae12e2c
thatsIch <thatsIch@mail.de>
2016-12-13 22:05:19 +0100

    Add levenshtein docstring

--- completion/levenshtein.py
+++ completion/levenshtein.py
@@ -1,4 +1,10 @@
 def levenshtein(top_string, bot_string):
+    """
+    the Levenshtein distance is a string metric for measuring the difference between two sequences.
+    Informally, the Levenshtein distance between two words is the minimum number of single-character edits
+    (i.e. insertions, deletions or substitutions) required to change one word into the other.
+    """
+
     if len(top_string) < len(bot_string):
         return levenshtein(bot_string, top_string)
 
