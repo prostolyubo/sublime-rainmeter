7feb1cb583cda7c3da54ff8a29fedd06ed46ae86
thatsIch <thatsIch@mail.de>
2016-12-14 21:46:46 +0100

    Added docstrings to test for program path provider

--- tests/test_path_program_path_provider.py
+++ tests/test_path_program_path_provider.py
@@ -1,37 +1,43 @@
+"""This module is to test the program path provider."""
+
 import sys
 
 from unittest import TestCase
 
-program_path_provider = sys.modules["Rainmeter.path.program_path_provider"]
+__PROGRAM_PATH_PROVIDER = sys.modules["Rainmeter.path.program_path_provider"]
 
 
 class TestProgramPathProvider(TestCase):
+    """Need to use a class to extend from TestCase."""
 
     def test_default_path(self):
-        """
-        If the user installed the application into the default directory
+        r"""
+        If the user installed the application into the default directory.
+
         it is in "C:/Program Files/Rainmeter"
         """
-        default_program_path = program_path_provider._get_rm_path_from_default_path()
+        default_program_path = __PROGRAM_PATH_PROVIDER._get_rm_path_from_default_path()
 
         self.assertEqual(default_program_path, "C:\\Program Files\\Rainmeter")
 
     def test_from_registry(self):
-        """
+        r"""
         Upon normal installation it will leave a registry entry to detect.
+
         We can use this to find the actual Rainmeter.
         Since we use the default installation path, there should no difference
         """
-        registry_program_path = program_path_provider._get_rm_path_from_registry()
+        registry_program_path = __PROGRAM_PATH_PROVIDER._get_rm_path_from_registry()
 
         self.assertEqual(registry_program_path, "C:\\Program Files\\Rainmeter")
 
     def test_overall(self):
-        """
-        Per default we install it onto "C:/Program Files/Rainmeter"
-        but since we use the path internally we already add / to it
+        r"""
+        Per default we install it onto "C:/Program Files/Rainmeter".
+
+        But since we use the path internally we already add / to it
         and python internal path uses \\ instead windows /
         """
-        program_path = program_path_provider.get_cached_program_path()
+        program_path = __PROGRAM_PATH_PROVIDER.get_cached_program_path()
 
         self.assertEqual(program_path, "C:\\Program Files\\Rainmeter\\")
