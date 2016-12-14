5ab0e32f3a3b49747b6035cee6dcc1002b5075e1
thatsIch <thatsIch@mail.de>
2016-12-14 21:40:20 +0100

    Add docstring to program drive provider

--- path/program_drive_provider.py
+++ path/program_drive_provider.py
@@ -1,3 +1,12 @@
+"""
+This module provides methods for determine the program drive.
+
+Rainmeter has an built-in variable called #PROGRAMDRIVE#.
+With this you can directly route to the drive in which Rainmeter is contained.
+If by some chance people use @Include on #PROGRAMDRIVE# it is still able to resolve
+the path and open the include for you.
+"""
+
 import os
 from functools import lru_cache
 
@@ -8,8 +17,7 @@ from .program_path_provider import get_cached_program_path
 
 @lru_cache(maxsize=None)
 def get_cached_program_drive():
-    """Get the value of the #PROGRAMDRIVE# variable"""
-
+    """Get the value of the #PROGRAMDRIVE# variable."""
     rainmeterpath = get_cached_program_path()
 
     if not rainmeterpath:
@@ -18,7 +26,11 @@ def get_cached_program_drive():
 
     probe_drive = os.path.splitdrive(rainmeterpath)
     if not probe_drive:
-        logger.info(__file__, "get_cached_program_drive", "Drive could not be extracted from '" + rainmeterpath + "'.")
+        logger.info(
+            __file__,
+            "get_cached_program_drive",
+            "Drive could not be extracted from '" + rainmeterpath + "'."
+        )
         return
 
     # can be either a drive like C:\
