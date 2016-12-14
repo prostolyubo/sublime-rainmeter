4909d895e9b9b3ca45e4def99663c0b2dd882994
thatsIch <thatsIch@mail.de>
2016-12-14 21:41:31 +0100

    Added docstring to addon path provider

--- path/addon_path_provider.py
+++ path/addon_path_provider.py
@@ -1,3 +1,12 @@
+"""
+This module provides methods for determine the addon path.
+
+Rainmeter has an built-in variable called #ADDONSPATH#.
+With this you can directly route to the drive in which Rainmeter is contained.
+If by some chance people use @Include on #ADDONSPATH# it is still able to resolve
+the path and open the include for you.
+"""
+
 import os.path
 from functools import lru_cache
 
@@ -8,10 +17,7 @@ from .setting_path_provider import get_cached_setting_path
 
 @lru_cache(maxsize=None)
 def get_cached_addon_path():
-    """
-    Get the value of the #ADDONSPATH# variable
-    """
-
+    """Get the value of the #ADDONSPATH# variable."""
     settingspath = get_cached_setting_path()
     if not settingspath:
         logger.error(
