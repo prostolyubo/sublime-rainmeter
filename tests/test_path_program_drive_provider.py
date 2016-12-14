e58e75d425af69771d08cb27483fe413ea8925f2
thatsIch <thatsIch@mail.de>
2016-12-14 21:56:58 +0100

    Added docstring to program drive provider test

--- tests/test_path_program_drive_provider.py
+++ tests/test_path_program_drive_provider.py
@@ -1,16 +1,18 @@
+"""This module contains the tests for the program drive provider."""
+
+
 import sys
 
 from unittest import TestCase
 
-program_drive_provider = sys.modules["Rainmeter.path.program_drive_provider"]
+__PROGRAM_DRIVE_PROVIDER = sys.modules["Rainmeter.path.program_drive_provider"]
 
 
 class TestFunctions(TestCase):
+    """Testing the proram drive provider with unitttest."""
 
     def test_default_drive(self):
-        """
-        Per default we install it onto c:/
-        """
-        program_drive = program_drive_provider.get_cached_program_drive()
+        """Per default we install it onto c:/."""
+        program_drive = __PROGRAM_DRIVE_PROVIDER.get_cached_program_drive()
 
         self.assertEqual(program_drive, "C:")
