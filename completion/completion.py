31321021cf3d84643621bb0db0da43e3d2bc5c64
thatsIch <thatsIch@mail.de>
2016-12-18 17:07:04 +0100

    Improved docstrings and formatting

--- completion/completion.py
+++ completion/completion.py
@@ -1,3 +1,5 @@
+"""This module handles the whole auto smart completion sublime-rainmeter has to offer."""
+
 # python libs
 import re
 
@@ -6,14 +8,15 @@ import sublime
 
 # own libs
 from .. import logger
-from ..completion.skin.rainmeter_section import SkinRainmeterSectionAutoComplete
-from ..completion.skin.metadata_section import SkinMetadataSectionAutoComplete
-from ..completion.section import SkinSectionAutoCompleter
+from .skin.rainmeter_section import SkinRainmeterSectionAutoComplete
+from .skin.metadata_section import SkinMetadataSectionAutoComplete
+from .section import SkinSectionAutoCompleter
 
 
 class ContextSensAutoCompletion(object):
     """
     This represents the internal implementation for the contextual auto completion.
+
     It uses smart environmental information like section, key, values etc
     to provide smarter auto completion suggestions.
     """
@@ -37,11 +40,13 @@ class ContextSensAutoCompletion(object):
     key_value_expression = re.compile(r'^\s*(.+?)\s*\=\s*(.*?)\s*$', re.MULTILINE)
 
     def __init__(self):
+        """Initialize the different completer components."""
         self.section = SkinSectionAutoCompleter()
         self.skin_rainmeter_section = SkinRainmeterSectionAutoComplete()
         self.skin_metadata_section = SkinMetadataSectionAutoComplete()
 
     def get_lines_of_section_on_cursor(self, view, location):
+        """Determine content of current section."""
         size = view.size()
         start_content = view.substr(sublime.Region(0, location))
         end_content = view.substr(sublime.Region(location, size))
@@ -56,10 +61,10 @@ class ContextSensAutoCompletion(object):
 
     def get_start_index_of_section(self, start_content):
         """
-        This returns the index of the section.
+        Return the index of the section.
+
         If no section is found the first index (0) is returned
         """
-
         matches = list(self.bracket_expression.finditer(start_content))
 
         if len(matches) > 0:
@@ -72,10 +77,10 @@ class ContextSensAutoCompletion(object):
 
     def get_end_index_of_section(self, end_content, offset, end_index):
         """
-        This returns the index of the next section.
+        Return the index of the next section.
+
         If no next section is found the last index is returned given through the param end_index
         """
-
         matches = list(self.bracket_expression.finditer(end_content))
         if len(matches) > 0:
             first_match = matches[0]
@@ -88,6 +93,7 @@ class ContextSensAutoCompletion(object):
     def get_key_value(self, line_content):
         """
         Extract the key and/or value in a line if existing.
+
         This is used if a specific completion is only shown
         on special conditions like only show X on measures.
 
@@ -97,21 +103,28 @@ class ContextSensAutoCompletion(object):
         if key_value_match:
             key_match = key_value_match.group(1)
             value_match = key_value_match.group(2)
-            logger.info(__file__, "on_query_completions", "key/value found in '" + line_content + "' with ('" + key_match + "', '" + value_match + "')")
+            logger.info(
+                __file__,
+                "on_query_completions",
+                "key/value found in '" + line_content + 
+                "' with ('" + key_match + "', '" + value_match + "')"
+            )
 
             return key_match, value_match
 
         key_only_match = self.key_expression.search(line_content)
         if key_only_match:
-            logger.info(__file__, "on_query_completions", "potential key found in '" + line_content + "'")
+            logger.info(
+                __file__,
+                "on_query_completions",
+                "potential key found in '" + line_content + "'"
+            )
             return key_only_match.group(1), None
 
         return None, None
 
     def get_key_values(self, lines):
-        """
-        Extract all key values in the given lines
-        """
+        """Extract all key values in the given lines."""
         key_values = []
 
         for line in lines:
@@ -122,14 +135,13 @@ class ContextSensAutoCompletion(object):
         return key_values
 
     def on_query_completions(self, view, prefix, locations):
-        """"
-        executed if a auto completion is requested.
+        """"Execute if a auto completion is requested.
+
         can be either via typing or manual invoked with ctrl+space.
 
         This provides general variable extractions which are then
         passed to the domain specific worker completions.
         """
-
         for location in locations:
             # ignore non scope
             if not view.match_selector(location, self.scope):
@@ -176,21 +188,44 @@ class ContextSensAutoCompletion(object):
             key_values = self.get_key_values(lines)
 
             if value_match == "":
-                logger.info(__file__, "on_query_completions", "after equal trigger in '" + line_content + "'")
+                logger.info(
+                    __file__,
+                    "on_query_completions",
+                    "after equal trigger in '" + line_content + "'"
+                )
                 # value trigger
-                value_result = self.skin_rainmeter_section.get_value_context_completion(section, key_match)
+                value_result = self.skin_rainmeter_section.get_value_context_completion(
+                    section,
+                    key_match
+                )
+
                 if value_result:
                     return value_result
 
             # only do key completion if we are in the key are
             # that means in front of the equal or no equal at all
             else:
-                logger.info(__file__, "on_query_completions", "before equal trigger in '" + line_content + "'")
-                key_result = self.skin_rainmeter_section.get_key_context_completion(prefix, line_content, section, key_values)
+                logger.info(
+                    __file__,
+                    "on_query_completions",
+                    "before equal trigger in '" + line_content + "'"
+                )
+                key_result = self.skin_rainmeter_section.get_key_context_completion(
+                    prefix,
+                    line_content,
+                    section,
+                    key_values
+                )
+
                 if key_result:
                     return key_result
 
-                key_result = self.skin_metadata_section.get_key_context_completion(prefix, line_content, section, key_values)
+                key_result = self.skin_metadata_section.get_key_context_completion(
+                    prefix,
+                    line_content,
+                    section,
+                    key_values
+                )
                 if key_result:
                     return key_result
 
