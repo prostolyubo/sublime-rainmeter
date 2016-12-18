c3fd5ce086295a489db2dc23b9cf6770f8cd7c23
thatsIch <thatsIch@mail.de>
2016-12-18 19:23:33 +0100

    Invalid constant names

--- lib/linux_colorpicker.py
+++ lib/linux_colorpicker.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python
+"""In case somebody wants to use the color picker on linux."""
+
+import sys
 
 import pygtk
 import gtk
-import sys
+
 
 pygtk.require('2.0')
 
 
-color_sel = gtk.ColorSelectionDialog("Sublime Color Picker")
+COLOR_SEL = gtk.ColorSelectionDialog("Sublime Color Picker")
 
 if len(sys.argv) > 1:
     if gtk.gdk.Color(sys.argv[1]):
-        color_sel.colorsel.set_current_color(gtk.gdk.Color(sys.argv[1]))
+        COLOR_SEL.colorsel.set_current_color(gtk.gdk.Color(sys.argv[1]))
 
-if color_sel.run() == gtk.RESPONSE_OK:
-    color = color_sel.colorsel.get_current_color()
+if COLOR_SEL.run() == gtk.RESPONSE_OK:
+    COLOR = COLOR_SEL.colorsel.get_current_color()
     # Convert to 8bit channels
-    red = color.red / 256
-    green = color.green / 256
-    blue = color.blue / 256
+    RED = COLOR.red / 256
+    GREEN = COLOR.green / 256
+    BLUE = COLOR.blue / 256
     # Convert to hexa strings
-    red = str(hex(red))[2:]
-    green = str(hex(green))[2:]
-    blue = str(hex(blue))[2:]
+    RED = str(hex(RED))[2:]
+    GREEN = str(hex(GREEN))[2:]
+    BLUE = str(hex(BLUE))[2:]
     # Format
-    if len(red) == 1:
-        red = "0%s" % red
-    if len(green) == 1:
-        green = "0%s" % green
-    if len(blue) == 1:
-        blue = "0%s" % blue
+    if len(RED) == 1:
+        RED = "0%s" % RED
+    if len(GREEN) == 1:
+        GREEN = "0%s" % GREEN
+    if len(BLUE) == 1:
+        BLUE = "0%s" % BLUE
 
-    final_color = red + green + blue
-    print(final_color.upper())
+    FINAL_COLOR = RED + GREEN + BLUE
+    print(FINAL_COLOR.upper())
 
-color_sel.destroy()
+COLOR_SEL.destroy()
