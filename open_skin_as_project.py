a028f7439bdbe7685bb0724011902efa21776559
thatsIch <thatsIch@mail.de>
2017-01-31 21:38:36 +0100

    Closes #34: Added open skin as project command

---
+++ open_skin_as_project.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+import sublime
+import sublime_plugin
+
+from .path.skin_path_provider import get_cached_skin_path
+
+
+class RainmeterOpenSkinAsProjectCommand(sublime_plugin.ApplicationCommand):
+
+    def run(self):
+        skins_path = get_cached_skin_path()
+        skins = os.listdir(skins_path)
+
+        sublime.active_window().show_quick_panel(skins, self.on_skin_selected, 0, 0, None)
+
+    def on_skin_selected(self, selected_skin_id):
+        skins_path = get_cached_skin_path()
+        skins = os.listdir(skins_path)
+        selected_skin = skins[selected_skin_id]
+        selected_skin_path = os.path.join(skins_path, selected_skin)
+
+        # to open a folder in new window, just create a new process with the folder as argument
+        st_path = sublime.executable_path()
+        subprocess.Popen([
+            st_path,
+            selected_skin_path
+        ])
