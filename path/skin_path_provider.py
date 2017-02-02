747c843e9c1f4b7e4df673b31c8c3f95937f8cd8
thatsIch <thatsIch@mail.de>
2017-02-02 23:10:39 +0100

    Split skin path provider into smaller logics

--- path/skin_path_provider.py
+++ path/skin_path_provider.py
@@ -1,4 +1,5 @@
-"""This module is about path resolving for the skin path.
+"""
+This module is about path resolving for the skin path.
 
 This is particular useful if you for example try create new skins
 or open an existing one.
@@ -20,10 +21,8 @@ from .program_path_provider import get_cached_program_path
 from .setting_path_provider import get_cached_setting_path
 
 
-@lru_cache(maxsize=None)
-def get_cached_skin_path():
-    """Get the value of the #SKINSPATH# variable."""
-    # First try to load the value from the "rainmeter_skins_path" setting
+def get_path_from_st_settings():
+    """Load the value from the "rainmeter_skins_path" setting."""
     loaded_settings = sublime.load_settings("Rainmeter.sublime-settings")
     skinspath = loaded_settings.get("rainmeter_skins_path", None)
 
@@ -34,14 +33,15 @@ def get_cached_skin_path():
         logger.info("Skins path '" + skinspath + "' found in sublime-settings file.")
         return os.path.normpath(skinspath) + "\\"
 
+
+def get_path_from_rm_settings(rm_path, settings_path):
+    """We check the Rainmeter.ini for the installation path."""
     # If it's not set, try to detect it automagically
-    rainmeterpath = get_cached_program_path()
-    settingspath = get_cached_setting_path()
-    if not rainmeterpath or not settingspath:
+    if not rm_path or not settings_path:
         return
 
     # First, try to read the SkinPath setting from Rainmeter.ini
-    fhnd = io.open(os.path.join(settingspath, "Rainmeter.ini"))
+    fhnd = io.open(os.path.join(settings_path, "Rainmeter.ini"))
     lines = fhnd.read()
     fhnd.close()
 
@@ -70,19 +70,24 @@ def get_cached_skin_path():
         logger.info("Skins path found in Rainmeter.ini.")
         return match.group("skinpath").strip().replace("/", "\\")
 
-    # if it's not found in the settings file, try to guess it
 
-    # If program path and setting path are equal, we have a portable
-    # installation. In this case, the Skins folder is inside the rainmeter
-    # path
-    if os.path.samefile(rainmeterpath, settingspath):
+def get_path_from_portable_rm_installation(rm_path, settings_path):
+    """
+    If program path and setting path are equal, we have a portable installation.
+
+    In this case, the Skins folder is inside the rainmeter path.
+    """
+    if os.path.samefile(rm_path, settings_path):
         logger.info("Skin path found in #PROGRAMPATH#" +
                     " because portable installation")
-        return os.path.join(rainmeterpath, "Skins") + "\\"
+        return os.path.join(rm_path, "Skins") + "\\"
 
-    # If it's not a portable installation, we try looking into the "My
-    # Documents" folder Since it could be relocated by the user, we have to
-    # query its value from the registry
+
+def get_path_from_registry():
+    """
+    If it's not a portable installation, we try looking into the "My Documents" folder.
+
+    Since it could be relocated by the user, we have to query its value from the registry."""
     try:
         regkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
                                 r"Software\Microsoft\Windows" +
@@ -103,8 +108,10 @@ def get_cached_skin_path():
     except OSError:
         pass
 
-    # If the value could not be retrieved from the registry,
-    # we try some educated guesses about default locations
+
+def guess_path_from_user_documents():
+    """If the value could not be retrieved from the registry, we try some educated guesses about default locations."""
+
     try:
         username = getpass.getuser()
     except Exception:  # pylint: disable=W0703; The docs to not specify the exception type
@@ -135,3 +142,38 @@ def get_cached_skin_path():
         logger.info("Skin path guessed from user name" +
                     " and Windows version")
         return os.path.join(mydocuments, "Rainmeter\\Skins") + "\\"
+
+
+@lru_cache(maxsize=None)
+def get_cached_skin_path():
+    """
+    Get the value of the #SKINSPATH# variable.
+
+    This can be collected from various installation locations,
+    since there are numerous ways to install Rainmeter.
+
+    The easiest solution is, if the user tells Sublime Rainmeter,
+    that he installed Rainmeter in a specific folder. We trust,
+    that the user knows where he installed it.
+
+    If we know, where the rainmeter settings are,
+    we can parse that from the Raimeter.ini.
+
+    If the user chose a portable installation,
+    it is also directly in the Rainmeter installation path.
+
+    If the user chose a default installation,
+    and enabled Rainmeter to store the information in the registry,
+    we can use that to determine the path.
+
+    If all fails we start guessing from the user document folder upwards.
+    """
+
+    rm_path = get_cached_program_path()
+    settings_path = get_cached_setting_path()
+
+    return get_path_from_st_settings() \
+        or get_path_from_rm_settings(rm_path, settings_path) \
+        or get_path_from_portable_rm_installation(rm_path, settings_path) \
+        or get_path_from_registry() \
+        or guess_path_from_user_documents()
