e6133ad5002c3a3eac6424ed9a4a9391f98826fa
thatsIch <thatsIch@mail.de>
2017-02-02 08:35:54 +0100

    Refactor open skin as project as function

--- open_skin_as_project.py
+++ open_skin_as_project.py
@@ -17,6 +17,30 @@ import sublime_plugin
 from .path.skin_path_provider import get_cached_skin_path
 
 
+def on_skin_selected(selected_skin_id):
+    """
+    This is a callback upon user selecting a skin.
+
+    This can handle user canceling the input.
+    Upon selection a respective path is evaluated
+    and send to a new sublime text instance.
+    """
+    if selected_skin_id == -1:
+        return
+
+    skins_path = get_cached_skin_path()
+    skins = os.listdir(skins_path)
+    selected_skin = skins[selected_skin_id]
+    selected_skin_path = os.path.join(skins_path, selected_skin)
+
+    # to open a folder in new window, just create a new process with the folder as argument
+    st_path = sublime.executable_path()
+    subprocess.Popen([
+        st_path,
+        selected_skin_path
+    ])
+
+
 class RainmeterOpenSkinAsProjectCommand(sublime_plugin.ApplicationCommand):
     """You can execute this command via the sublime API like sublime.run_command("rainmeter_open_skin_as_project")."""
 
@@ -25,27 +49,4 @@ class RainmeterOpenSkinAsProjectCommand(sublime_plugin.ApplicationCommand):
         skins_path = get_cached_skin_path()
         skins = os.listdir(skins_path)
 
-        sublime.active_window().show_quick_panel(skins, self.on_skin_selected, 0, 0, None)
-
-    def on_skin_selected(self, selected_skin_id):
-        """
-        This is a callback upon user selecting a skin.
-
-        This can handle user canceling the input.
-        Upon selection a respective path is evaluated
-        and send to a new sublime text instance.
-        """
-        if selected_skin_id == -1:
-            return
-
-        skins_path = get_cached_skin_path()
-        skins = os.listdir(skins_path)
-        selected_skin = skins[selected_skin_id]
-        selected_skin_path = os.path.join(skins_path, selected_skin)
-
-        # to open a folder in new window, just create a new process with the folder as argument
-        st_path = sublime.executable_path()
-        subprocess.Popen([
-            st_path,
-            selected_skin_path
-        ])
+        sublime.active_window().show_quick_panel(skins, on_skin_selected, 0, 0, None)
