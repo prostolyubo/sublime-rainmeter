4cbd2b00dd7f7b7fd035d33088c45a3552d8ccb1
thatsIch <thatsIch@mail.de>
2017-03-11 08:29:17 +0100

    Added missing docstrings

--- http/content_downloader.py
+++ http/content_downloader.py
@@ -1,6 +1,10 @@
+"""Simple module to download files from a remote location."""
+
+
 import urllib.request
 
 
 def download_from_to(from_location, to_file_path):
+    """Simple access wrapper to download from a remote location."""
     # urllib.request.urlretrieve('http://example.com/big.zip', 'file/on/disk.zip')
     urllib.request.urlretrieve(from_location, to_file_path)
