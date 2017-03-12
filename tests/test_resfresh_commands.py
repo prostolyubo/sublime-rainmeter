2cdf78f7dc22f306cf841c9c7e6ce831b9028bc4
thatsIch <thatsIch@mail.de>
2017-03-12 12:29:06 +0100

    Rename test methods

--- tests/test_resfresh_commands.py
+++ tests/test_resfresh_commands.py
@@ -12,14 +12,14 @@ class TestRefreshCommandArgs(TestCase):
 
     # pylint: disable=W0703; This is acceptable since we are testing it not failing
 
-    def test_w_activate_inc_should_be_shorter(self):
-        """."""
+    def test_for_more_flags(self):
+        """Calling from an inc file should result in less call flags."""
         shorter = REFRESH_COMMANDS.calculate_refresh_commands("Rainmeter.exe", "test-config", "file.inc", True, True)
         longer = REFRESH_COMMANDS.calculate_refresh_commands("Rainmeter.exe", "test-config", "file.ini", True, False)
 
         self.assertGreater(longer, shorter)
 
-    def test_w_activate_ini_one_larger(self):
+    def test_for_size(self):
         """Calling an inc file should matter only by one argument.
 
         It is missing the file name since we cannot directly refresh the inc file."""
@@ -29,8 +29,8 @@ class TestRefreshCommandArgs(TestCase):
 
         self.assertEqual(len(inc) + 1, len(ini))
 
-    def test_without_activate_ini_inc_same(self):
-        """."""
+    def test_config_overwrite(self):
+        """If the config option is not activated we force it to do nothing thus resulting in the same result."""
         inc = REFRESH_COMMANDS.calculate_refresh_commands("Rainmeter.exe", "test-config", "file.inc", False, True)
         ini = REFRESH_COMMANDS.calculate_refresh_commands("Rainmeter.exe", "test-config", "file.ini", False, False)
 
